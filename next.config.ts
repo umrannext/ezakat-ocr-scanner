2a0057a34802c53b36930a19cd9eb2b6a2465bef
umrannext-297 <umrannext@gmail.com>
2026-09-23 19:03:46 +0800

    fix: trace pg-cloudflare dist files in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
